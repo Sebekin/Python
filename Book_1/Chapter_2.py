116656311b70446930e4af25f265fa8116375b40
Aleksey Sebekin <sebekinav@gmail.com>
2019-01-28 13:38:33 +0300

    Chapter_2

--- Book_1/Chapter_2.py
+++ Book_1/Chapter_2.py
@@ -3,3 +3,92 @@ print(message)
 
 message = "Hello Python Crash Course world"
 print(message)
+
+#
+
+print('I told my friend, "Python is my favorite language"')
+print("The language 'Python' is named after Monty Python, not the snake")
+print("One of Pyhon's strength is its diverse and supportive comunity")
+
+#
+
+name = "ada lovelace"
+print(name.title())
+
+#
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+#
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+
+#
+
+print("Python")
+print("\tPython")
+
+#
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#
+
+favorit_language = 'python '
+favorit_language
+
+favorit_language.rstrip()
+favorit_language
+
+#
+
+favorit_language = ' python '
+
+favorit_language.rstrip()
+
+favorit_language.lstrip()
+
+favorit_language.strip()
+
+#
+
+2 + 3
+3 - 2
+2 * 3
+3 / 2
+3 ** 2
+3 ** 3
+10 ** 6
+2 + 3 * 4
+(2 + 3) * 4
+
+#
+
+0.1 + 0.1
+0.2 + 0.2
+2 * 0.1
+2 * 0.2
+0.2 + 0.1
+3 * 0.1
+
+#
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
+
+#
+
+# Say hello to everyone
+print("Hello Python people!")
+
+#
+
+import this
